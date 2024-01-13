3368fb10a71b1ed7496a833ce5d6fac01757a623
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2024-01-12 22:07:03 -0800

    Working Non Working HW1 Dynamic Array
    Running in to a problem with the dynamic array not working properly. Logic works when not using a dynamic array.

--- HW1-ProgROM/ProgROM/ProgROM.srcs/sim_1/new/ProgRom_TB.sv
+++ HW1-ProgROM/ProgROM/ProgROM.srcs/sim_1/new/ProgRom_TB.sv
@@ -23,14 +23,18 @@
 module ProgRom_TB();
     // Inputs
     logic PROG_CLK_TB;
-    logic [15:0] PROG_ADDR_TB;
+    logic [31:0] PROG_ADDR_TB;
 
     // Outputs
     logic [31:0] INSTRUCT_TB;
 
     // Logic
+    // (* rom_style="{distributed | block}" *)
+	// // (* ram_decomp = "power" *) logic [31:0] rom_TB [0:16383];
+	// (* ram_decomp = "power" *) logic [31:0] rom_TB [0:6];
+
     logic [31:0] rom_TB [$];
-    logic [13:0] length_TB;
+    logic pass;
 
     // Instantiate the Unit Under Test (UUT)
     ProgRom uut (
@@ -42,19 +46,28 @@ module ProgRom_TB();
     initial begin
         PROG_CLK_TB = 0;
         $readmemh("otter_memory.mem", rom_TB);
+        pass = 1;
         for (int i = 0; i < rom_TB.size(); i++) begin
-            // PROG_ADDR_TB = 32'h0000_0000 + (i*4);
-            // #20;
-            // $display("rom[%0d]: %h", i, INSTRUCT_TB);
+        // for (int i = 0; i < 6; i++) begin
+            PROG_ADDR_TB = 32'h0000_0000 + (i*4);
+            #20;
+            $display("rom[%0d]: %h", i, INSTRUCT_TB);
             $display("rom_tb[%0d]: %h", i, rom_TB[i]);
 
-            // if (INSTRUCT_TB == (32'h0000_0000 + rom_TB[i])) begin
-            //     $display("Match rom_TB[%0d] = %h", i, rom_TB[i]);
-            // end
-            // #20;
+            if (INSTRUCT_TB == (32'h0000_0000 + rom_TB[i])) begin
+                $display("Match rom_TB[%0d] = %h", i, rom_TB[i]);
+            end else begin
+                $display("No Match rom_TB[%0d] = %h", i, rom_TB[i]);
+                pass = 0;
+            end
+            #20;
         end
 
-
+        if (pass == 1) begin
+            $display("OVERALL PASS");
+        end else begin
+            $display("OVERALL FAIL");
+        end 
     end
 
     always #5 PROG_CLK_TB = ~PROG_CLK_TB;
