747565e5de83e088990e0b17960cca25f48c2240
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2024-01-29 20:54:49 -0800

    Fixed Formatting and found error

--- SW3-DecisionTree/SW3-Delay.asm
+++ SW3-DecisionTree/SW3-Delay.asm
@@ -1,21 +1,24 @@
     # Setup registers 
-    lui     x5, 0x00000 # Output value from multiplication
-    li      x6, 5000 # Define delay time by cycle count
+    lui     x11, 0x11000    # Define Address for switch input
+    lui     x5, 0x00000     # Output value from multiplication
+
+    li      x6, 5000        # Define delay time by cycle count
     # Calculation of the cycle count:
     # Each instruction takes 40ns noting that there are 4 
     # instructions in the count loop this means that one loop
     # takes 160ns. Divideing 0.5 seconds by the 160ns results
     # in 3,125,000 cycles.
-    lui     x11, 0x11000 # Define Address for switch input
 
     # Recive user input and format
-    lw      x9, 0(x11) # Load switch values to x9
+    lw      x9, 0(x11)      # Load switch values to x9
 
 countLoop:
+    # Count up untill the desired time is met and the move
+    # forward in the program
     bge     x5, x6, end
-    addi    x5, x5, 1
+    addi    x5, x5, 1       # Increment loop counter
     nop
     j       countLoop
 
 end:
-    sw      x9, 0x40(x11) # Store final value
+    sw      x9, 0x40(x11)   # Store final value
--- SW3-DecisionTree/SW3-Multiplication.asm
+++ SW3-DecisionTree/SW3-Multiplication.asm
@@ -1,19 +1,19 @@
     # Setup registers 
-    lui     x5, 0x00000 # Output value from multiplication
-    lui     x11, 0x11000 # Define Address for switch input
-    lui     x9, 0x00000 # Current place counter
+    lui     x5, 0x00000         # Output value from multiplication
+    lui     x11, 0x11000        # Define Address for switch input
+    lui     x9, 0x00000         # Current place counter
     addi    x10, x0, 0x00000010 # Number of places to multiply
 
     # Recive user input and format
-    lw      x6, 0(x11) # Load switch values to x6
-    add     x7, x0, x6 # Load switch values to x7
-    srli    x7, x7, 16 # Isolate second number
+    lw      x6, 0(x11)          # Load switch values to x6
+    add     x7, x0, x6          # Load switch values to x7
+    srli    x7, x7, 16          # Isolate second number
 
 multiplyStep:
     # Begin multiplication
-    bge     x9, x10, end # If x9 >= x10 then end
-    andi    x13, x6, 1 # Check is LSB is 1
-    beqz    x13, isZero # Branch if LSB is 0
+    bge     x9, x10, end        # If x9 >= x10 then end
+    andi    x13, x6, 1          # Check is LSB is 1
+    beqz    x13, isZero         # Branch if LSB is 0
     
     # Add shifted value to running total and then shift 
     # registers and increment index
@@ -32,4 +32,4 @@ isZero:
     
 
 end:
-    sw      x5, 0x40(x11) # Store final value
+    sw      x5, 0x40(x11)       # Store final value
