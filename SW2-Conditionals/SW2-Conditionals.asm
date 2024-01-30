99f807bed3d9f4eaf1e1c86c6b7b7693efd57fd6
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2024-01-30 11:53:32 -0800

    SW2 Final Commit

--- SW2-Conditionals/SW2-Conditionals.asm
+++ SW2-Conditionals/SW2-Conditionals.asm
@@ -1,13 +1,13 @@
 	lui  x30, 0x11000
-	lw   x5, 0(x30) 	# load the switches in to memory
+	lw   x5, 0(x30) 		# load the switches in to memory
 	li   x6, 0x00008000 	# load the comparison number in to memory
 
 	bge  x5, x6, isGreater 	# check is number in switches is >= 32768
-	slli x5, x5, 1  	# multiply by 2 doing a shift 1 bit
+	slli x5, x5, 1  		# multiply by 2 doing a shift 1 bit
 	j    end
 	
 isGreater:
-	srli x5, x5, 2		# divide by shifting 2 bits right
+	srli x5, x5, 2			# divide by shifting 2 bits right
 
 end:
-	sw   x5, 0x40(x30)	# store to sseg
+	sw   x5, 0x40(x30)		# store to sseg
